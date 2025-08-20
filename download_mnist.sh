572b2ffba948883f63f184005d00de1648237687
Hugh <hugh.lpd@gmail.com>
2025-08-20 22:57:14 +0800

    update mnist download script

--- download_mnist.sh
+++ download_mnist.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 DATAROOT="./data"
-[ -e $DATAROOT ] || mkdir $DATAROOT
+# [ -e $DATAROOT ] || mkdir $DATAROOT
 
 urls=("http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz" "http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz" "http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz" "http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz")
+
+git lfs install
+git clone https://huggingface.co/datasets/Minter/MNIST data
 
 for url in ${urls[@]}; do
   gz=$(basename $url)
   ugz=$(echo $gz | sed 's/\.gz//')
-  [ -e $DATAROOT/$gz ] || wget -c -O $DATAROOT/$gz $url
+  # [ -e $DATAROOT/$gz ] || wget -c -O $DATAROOT/$gz $url || curl -LO $DATAROOT/$gz $url
   [ -e $DATAROOT/$ugz ] || gunzip -k $DATAROOT/$gz
 done
